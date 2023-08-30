d08dee9a8bd6fec0dbb7d2a659e287e66420af9f
RamenRa <53000259+RamenRa@users.noreply.github.com>
2023-08-30 22:25:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,7 @@
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci  package/passwall
 #cp -rf passwall_package/*  package/passwall
 #rm -rf passwall_package
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
 
