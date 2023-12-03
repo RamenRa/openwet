3be65e0504659415e5742005714b90f6951d8f6a
RamenRa <53000259+RamenRa@users.noreply.github.com>
2023-12-03 21:28:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci  package/passwall
 #cp -rf passwall_package/*  package/passwall
 #rm -rf passwall_package
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # git clone https://github.com/wiwizcom/WiFiPortal.git package/WiFiPortal
 
